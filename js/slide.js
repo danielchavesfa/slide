b0c2335aca7aac8d0bed3958cf0ca0f60aeec201
danielchavesfa <danielchavesfa@hotmail.com>
2023-04-04 13:50:57 -0300

    adionado a logica do slide, events click e mousemove

---
+++ js/slide.js
@@ -0,0 +1,36 @@
+export default class Slide {
+  constructor(wrapper, slide) {
+    this.wrapper = document.querySelector(wrapper);
+    this.slide = document.querySelector(slide);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    this.wrapper.addEventListener('mousemove', this.onMove);
+  }
+
+  onEnd() {
+    this.wrapper.removeEventListener('mousemove', this.onMove);
+  }
+
+  onMove() {
+    
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener('mousedown', this.onStart);
+    this.wrapper.addEventListener('mouseup', this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
